1e6f0bfa50c4adfe82d066356a7ba1e9d4156c22
edwinsuubi <edwinsuubi@gmail.com>
2020-03-12 12:31:14 +0300

    Creates central navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light mynav">
+    <a class="navbar-brand" href="#">
+      <img
+        src="../assets/icon.png"
+        width="30"
+        height="30"
+        class="d-inline-block align-top"
+        alt="ASRAS"
+      />
+      ASRAS
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link">
+            <router-link to="/Login">Login</router-link>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">
+            <router-link to="/Signup">Signup</router-link>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navigation"
+};
+</script>
+
+<style scoped>
+.mynav {
+  background: linear-gradient(white, #80d0c7);
+  width: 100%;
+}
+li {
+  display: inline-block;
+}
+</style>
