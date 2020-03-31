84fef2a1057421f2d8c4d082f4eb8fc874be5e13
edwinsuubi <edwinsuubi@gmail.com>
2020-03-31 13:27:16 +0300

    Adds About view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,11 @@
 <template>
-  <div></div>
+  <div>
+    <p>This View will contain information about the ARMS Project</p>
+  </div>
 </template>
+<style scoped></style>
+<script>
+export default {
+  name: "About"
+};
+</script>
