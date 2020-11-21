0c44a7ba930fc9daea5fc31e5b3c20ebfb56dd12
edwinsuubi <edwinsuubi@gmail.com>
2020-11-21 15:16:38 +0300

    Imports display from store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import display from "./display";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  // plugins: [
+  // createPersistedState({
+  //   //   key: "vuex",
+  // })
+  // ],
+  modules: {
+    display
+  }
 });
